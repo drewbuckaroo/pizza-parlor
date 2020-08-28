9721e965c6170f5e279dc378b1ee6271b30f6638
drewbuckaroo <drewmlockhart@gmail.com>
2020-08-28 11:28:30 -0700

    add function to try and see returned pizza price.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,63 @@
 // Business Logic
 
-function Pizza(size, toppings) {
-  this.size = size;
-  this.toppings = toppings;
+function Pizza(pizzaSize, pizzaTopping1, pizzaTopping2, pizzaTopping3) {
+  this.pizzaSize = pizzaSize;
+  this.pizzaTopping1 = pizzaTopping1;
+  this.pizzaTopping2 = pizzaTopping2;
+  this.pizzaTopping3 = pizzaTopping3;
 
 }
 
 Pizza.prototype.price = function() {
-  if (this.size == "small") {
+  let price;
+  if (this.pizzaSize == "small") {
     price = 10;
-  } else if (this.size == "medium") {
+  } else if (this.pizzaSize == "medium") {
     price = 20;
-  } else if (this.size == "large") {
+  } else if (this.pizzaSize == "large") {
     price = 30;
   }
-  if (this.toppings == "pepperoni") {
+  if (this.pizzaTopping1 == "pepperoni") {
     price = price + 5;
+  } else if (this.pizzaTopping1 == "pineapple") {
+    price = price + 2;
+  } else if (this.pizzaTopping1 == "jalepeno") {
+      price = price + 2;
   }
+  if (this.pizzaTopping2 == "pepperoni") {
+    price = price + 5;
+  } else if (this.pizzaTopping2 == "pineapple") {
+    price = price + 2;
+  } else if (this.pizzaTopping2 == "jalepeno") {
+      price = price + 2;
+  }
+  if (this.pizzaTopping3 == "pepperoni") {
+    price = price + 5;
+  } else if (this.pizzaTopping3 == "pineapple") {
+    price = price + 2;
+  } else if (this.pizzaTopping3 == "jalepeno") {
+      price = price + 2;
+  }
+  this.price.push(price);
   return price;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 // User Interface Logic
 
 $(document).ready(function() {
+  $("form#pizzaSelection").submit(function(event) {
+    event.preventDefault();
+    const pizzaSize = $("#pizzaSize").val();
+    const pizzaTopping1 = $("#pizzaTopping1").val();
+    const pizzaTopping2 = $("#pizzaTopping2").val();
+    const pizzaTopping3 = $("#pizzaTopping3").val();
+
+    let newPizza = Pizza(pizzaSize, pizzaTopping1, pizzaTopping2, pizzaTopping3);
+    $("#price").text(price);
+    $("#price").show(newPizza.price());
+
+  });
  
 });
